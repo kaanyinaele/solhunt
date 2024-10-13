d8fefd8032a3094198acdf31de95412d97ce6345
kaanyinaele <kaanyinaele@gmail.com>
2024-10-13 15:55:07 +0100

    vercel dependency fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // Ensure all assets are served relative to the app's root
   plugins: [
     react({
-      jsxImportSource: '@emotion/react',
+      jsxImportSource: "@emotion/react",
       babel: {
-        plugins: ['@emotion/babel-plugin'],
+        plugins: ["@emotion/babel-plugin"],
       },
     }),
   ],
   resolve: {
     alias: {
-      '@emotion/core': '@emotion/react',
-      '@emotion/styled': '@emotion/styled',
-      '@mui/styled-engine': '@mui/styled-engine-sc',
+      "@emotion/core": "@emotion/react",
+      "@emotion/styled": "@emotion/styled",
+      "@mui/styled-engine": "@mui/styled-engine-sc",
     },
   },
-})
+});
